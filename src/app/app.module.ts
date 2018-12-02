e3f702f55efdf447b12910071b0ba971fe5235be
jugonzalez40 <juanmarfil9696@gmail.com>
2018-12-02 11:07:50 -0500

    meriasd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from './material.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +19,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     FlexLayoutModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable, } from 'rxjs';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.sass']
 })
 export class LoginComponent implements OnInit {
+  private giffs: Observable<any>;
+  private httpClient :HttpClient ;
+  private images :Array<String> = [];
+  constructor(httpClient: HttpClient) {
+    this.httpClient = httpClient;
 
-  constructor() { }
+
+
+  }
 
   ngOnInit() {
+    this.giffs = this.httpClient.get('https://api.giphy.com/v1/gifs/search?api_key=u3uWS5zfyE3kZ955zjJ3T2lV2f3yNjWv&q=random&limit=100&offset=0&rating=R&lang=en');
+
+    this.giffs.subscribe((data)=>{
+      data.data.forEach(d=>{
+        this.images.push(d.images.fixed_height_small.url);
+      });
+    })
   }
 
 }
